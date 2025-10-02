311f1b12602ead352e9c1856402c2ca846bb0c3a
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 16:06:57 -0300

    Add files via upload

---
+++ Exercicio 10/Exercicio10/Program.cs
@@ -0,0 +1,14 @@
+﻿double n1,n2, me = 0;
+Console.WriteLine("Digite sua 1° nota");
+n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite sua 1° nota");
+n2 = double.Parse(Console.ReadLine());
+me = (n1+n2)/2;
+if (me>=6) {Console.WriteLine("Aprovado")
+    ; }
+else
+{
+    Console.WriteLine("Reprovad");
+}
+
+
