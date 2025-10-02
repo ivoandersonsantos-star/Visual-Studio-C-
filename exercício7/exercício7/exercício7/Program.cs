6f0bea792f6d79ec8b7f46a071b3822dc11e07d5
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 14:14:23 -0300

    Add files via upload

---
+++ exercício7/exercício7/exercício7/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int n = 0;
+Console.WriteLine("Digite um numero");
+n = int.Parse(Console.ReadLine());
+if (n > 10) { Console.WriteLine($"{n} é maior que 10");}
+else if (n < 10) { Console.WriteLine($"{n} é menor que 10");}
+
+
