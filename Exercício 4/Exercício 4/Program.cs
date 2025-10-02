4f913fc97f2088ee1feb0536197991bdd443c509
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 16:11:55 -0300

    Add files via upload

---
+++ Exercício 4/Exercício 4/Program.cs
@@ -0,0 +1,5 @@
+﻿int id, an, d;
+Console.WriteLine("Digite a sua idade atual:");
+id = int.Parse(Console.ReadLine());
+d = id * 365;
+Console.WriteLine($"A Quantidade dias de vida com {id} anos de idade é: {d}");
