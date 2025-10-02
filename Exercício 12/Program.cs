52161beaf26cf16657f151833c58509a536ddbdc
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 16:38:16 -0300

    Add files via upload

---
+++ Exercício 12/Program.cs
@@ -0,0 +1,25 @@
+﻿int n1, n2, rep;
+rep = 0;
+
+while (rep < 1)
+{
+    Console.WriteLine("Escreva o primeiro número: ");
+    n1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Escreva o segundo número: ");
+    n2 = int.Parse(Console.ReadLine());
+
+    if (n1 > n2)
+    {
+        Console.WriteLine("O primeiro número é maior");
+        rep = 1;
+    }
+    if (n2 > n1)
+    {
+        Console.WriteLine("O segundo é maior");
+        rep = 1;
+    }
+    if (n1 == n2)
+    {
+        Console.WriteLine("Não é permitido números iguais, digite novamente:\n");
+    }
+}
