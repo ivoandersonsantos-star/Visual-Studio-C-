268b2af13e9b32acd012a95ffb799612a99f8def
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 14:45:13 -0300

    Add files via upload

---
+++ execício 8/execício 8/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = 0;
+Console.WriteLine("Digite um número");
+n = int.Parse(Console.ReadLine());
+if (n > 0) {
+    Console.WriteLine($"{n} é positivo");}
+else if (n==0) { Console.WriteLine("Número inválido");}
+else if (n<0) { Console.WriteLine($"{n} é negativo"); }
