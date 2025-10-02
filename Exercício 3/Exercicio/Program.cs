c65ef24a43ac884466b6e5ef02ed6e21770445ab
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 16:11:09 -0300

    Add files via upload

---
+++ Exercício 3/Exercicio/Program.cs
@@ -0,0 +1,6 @@
+﻿int b, h = 0;
+Console.WriteLine("DIgite a altura:");
+h = int.Parse(Console.ReadLine());
+Console.WriteLine("DIgite base:");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine($"A área do quadrado é: {b*h}");
