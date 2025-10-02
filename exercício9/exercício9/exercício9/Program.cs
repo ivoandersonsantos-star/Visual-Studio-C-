d93800af8779501555c5e3fa68242b90a142384d
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-02 14:46:55 -0300

    Add files via upload

---
+++ exercício9/exercício9/exercício9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.Design;
+
+int m = 0;
+double valorcompra = 0;
+Console.WriteLine(" Escreva a quantia de maçãs");
+m =int.Parse(Console.ReadLine());
+if (m <= 12) {
+    valorcompra = m;
+ Console.WriteLine($"Sua compra ficou R${valorcompra}");}
+else if (m > 12) {
+    valorcompra = m * 1.30;
+    Console.WriteLine($"Sua compra ficou R${valorcompra}")
+    ; }
