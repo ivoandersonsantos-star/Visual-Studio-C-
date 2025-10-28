2a5394d0b4bb01154eb784e0f03df33c8415dfb7
ivo_txt <ivoandersonsantos@gmail.com>
2025-10-28 17:12:25 -0300

    Add files via upload

---
+++ IMC/IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,425 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button13 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.Controls.Add(button13);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(32, 297);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(490, 411);
+            panel1.TabIndex = 0;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Orange;
+            button13.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button13.ForeColor = SystemColors.ActiveCaptionText;
+            button13.Location = new Point(49, 370);
+            button13.Name = "button13";
+            button13.Size = new Size(382, 23);
+            button13.TabIndex = 1;
+            button13.Text = "CÁLCULE";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Orange;
+            button10.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(329, 110);
+            button10.Name = "button10";
+            button10.Size = new Size(102, 63);
+            button10.TabIndex = 11;
+            button10.Text = "6";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Orange;
+            button11.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(190, 110);
+            button11.Name = "button11";
+            button11.Size = new Size(102, 63);
+            button11.TabIndex = 10;
+            button11.Text = "5";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Orange;
+            button12.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button12.Location = new Point(49, 110);
+            button12.Name = "button12";
+            button12.Size = new Size(102, 63);
+            button12.TabIndex = 9;
+            button12.Text = "4";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Orange;
+            button7.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(329, 193);
+            button7.Name = "button7";
+            button7.Size = new Size(102, 63);
+            button7.TabIndex = 8;
+            button7.Text = "3";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Orange;
+            button8.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(190, 287);
+            button8.Name = "button8";
+            button8.Size = new Size(102, 63);
+            button8.TabIndex = 7;
+            button8.Text = "0";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Orange;
+            button9.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(49, 287);
+            button9.Name = "button9";
+            button9.Size = new Size(102, 63);
+            button9.TabIndex = 6;
+            button9.Text = ",";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Orange;
+            button4.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(329, 287);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 63);
+            button4.TabIndex = 5;
+            button4.Text = "DEL";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Orange;
+            button5.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(190, 193);
+            button5.Name = "button5";
+            button5.Size = new Size(102, 63);
+            button5.TabIndex = 4;
+            button5.Text = "2";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Orange;
+            button6.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(49, 193);
+            button6.Name = "button6";
+            button6.Size = new Size(102, 63);
+            button6.TabIndex = 3;
+            button6.Text = "1";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Orange;
+            button3.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(329, 23);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 63);
+            button3.TabIndex = 2;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Orange;
+            button2.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(190, 23);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 63);
+            button2.TabIndex = 1;
+            button2.Text = "8";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Orange;
+            button1.Font = new Font("Segoe UI", 30F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(49, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 63);
+            button1.TabIndex = 0;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(400, 246);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 45);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(400, 179);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 44);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(400, 117);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 46);
+            textBox3.TabIndex = 14;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(327, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 32);
+            label1.TabIndex = 15;
+            label1.Text = "Peso";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(309, 179);
+            label2.Name = "label2";
+            label2.Size = new Size(85, 32);
+            label2.TabIndex = 16;
+            label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(327, 246);
+            label3.Name = "label3";
+            label3.Size = new Size(60, 32);
+            label3.TabIndex = 17;
+            label3.Text = "IMC";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(506, 191);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 32);
+            label4.TabIndex = 18;
+            label4.Text = "M";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(506, 131);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 32);
+            label5.TabIndex = 19;
+            label5.Text = "Kg";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(289, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 15);
+            label7.TabIndex = 21;
+            label7.Click += label7_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 30F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(99, -1);
+            label6.Name = "label6";
+            label6.Size = new Size(347, 54);
+            label6.TabIndex = 22;
+            label6.Text = "CÁLCULO DE IMC";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(32, 257);
+            label8.Name = "label8";
+            label8.Size = new Size(158, 37);
+            label8.TabIndex = 23;
+            label8.Text = "SITUAÇÂO:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label9.Location = new Point(12, 53);
+            label9.Name = "label9";
+            label9.Size = new Size(280, 37);
+            label9.TabIndex = 24;
+            label9.Text = "Informe Peso e Altura";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.icons8_balança_50;
+            pictureBox1.Location = new Point(280, 104);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(55, 59);
+            pictureBox1.TabIndex = 25;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.icons8_ruler_vertical_48;
+            pictureBox2.Location = new Point(264, 179);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(39, 50);
+            pictureBox2.TabIndex = 26;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Orange;
+            ClientSize = new Size(563, 702);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+        private Button button13;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Label label6;
+        private Label label8;
+        private Label label9;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ IMC/IMC/IMC/Form1.cs
@@ -0,0 +1,40 @@
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
